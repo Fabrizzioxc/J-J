1962e9f606288acad5285ad174d059e60548b946
Fabrizzioxc <fabrizziozv.17@gmail.com>
2025-07-10 01:26:02 -0500

    Feat: cambio de posicion

--- src/components/WhatsappButton.jsx
+++ src/components/WhatsappButton.jsx
@@ -3,7 +3,7 @@ import WhatsappIcon from './icons/WhatsappIcon.jsx';
 
 export default function WhatsappButton() {
   // Reemplaza con el número de WhatsApp de tu empresa (si aún no lo hiciste)
-  const whatsappNumber = '51987654321'; // Ejemplo: Cambia esto al número de J&J
+  const whatsappNumber = '51992198315'; // Ejemplo: Cambia esto al número de J&J
   const whatsappMessage = encodeURIComponent('Hola, me gustaría obtener más información sobre sus servicios.');
   const whatsappLink = `https://wa.me/${whatsappNumber}?text=${whatsappMessage}`;
 
@@ -12,7 +12,7 @@ export default function WhatsappButton() {
   href={whatsappLink}
   target="_blank"
   rel="noopener noreferrer"
-  className="fixed bottom-6 right-6 z-50 bg-[#25D366] text-white p-3 md:p-4 rounded-full shadow-lg hover:scale-110 transition-transform duration-300 ease-in-out flex items-center justify-center"
+  className="fixed bottom-15 right-15 z-50 bg-[#25D366] text-white p-3 md:p-4 rounded-full shadow-lg hover:scale-110 transition-transform duration-300 ease-in-out flex items-center justify-center"
   aria-label="Contactar por WhatsApp"
   title="Contactar por WhatsApp"
   data-astro-transition-persist="whatsapp-button"
